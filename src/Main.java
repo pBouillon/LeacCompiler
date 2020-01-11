94333bfc4e802b92ffc09cc194dee08817165045
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-01-11 15:19:29 +0100

    🌱 Add basic main file

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Compiler project's entry point
+ *
+ * @author Florian Vogt
+ * @author Pierre Bouillon
+ * @author Victor Varnier
+ *
+ * @url https://github.com/pBouillon/TELECOM_Trad
+ * @version 0.1
+ */
+public class Main {
+
+    /**
+     * Default entry point
+     * @param args program arguments
+     */
+    public static void main(String[] args) { }
+
+}
